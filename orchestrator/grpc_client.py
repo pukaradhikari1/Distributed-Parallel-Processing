3a48ec65b7687b6003fe474e27598cbd52e9c4f7
pukaradhikari1 <pukar.adhikari656@gmail.com>
2026-07-09 07:57:20 +0545

    dynamic routing done

--- orchestrator/grpc_client.py
+++ orchestrator/grpc_client.py
@@ -1,16 +1,25 @@
+import os
+import sys
 import grpc
-import distributed_pb2
-import distributed_pb2_grpc
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+grpc_path = os.path.abspath(os.path.join(current_dir, "..", "grpc_layer"))
+sys.path.append(grpc_path)
+
+import distributed_pb2 #type: ignore
+import distributed_pb2_grpc  # type: ignore
+
 
 def send_task_to_worker(worker_ip, job_id, shard_index, script_bytes, data_bytes, model_weights_bytes=b""):
-   
+    
+    # 500MB Limit for ML Weights
     MAX_MESSAGE_LENGTH = 500 * 1024 * 1024
     options = [
         ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
         ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH)
     ]
 
-    
+  
     channel = grpc.insecure_channel(f"{worker_ip}:50051", options=options)
     stub = distributed_pb2_grpc.WorkerServiceStub(channel)
 
@@ -23,7 +32,7 @@ def send_task_to_worker(worker_ip, job_id, shard_index, script_bytes, data_bytes
     )
 
     try:
-       
+      
         result = stub.ExecuteTask(payload, timeout=300)
         return result
 
